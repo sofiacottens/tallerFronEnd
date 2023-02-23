9f475f034fd216f1154e8ad010c9c84861c89b42
elii-fiorina <63215653+elii-fiorina@users.noreply.github.com>
2023-02-22 23:22:36 -0300

    mejoras registro

--- obli/src/components/Registrarse/Registrarse.jsx
+++ obli/src/components/Registrarse/Registrarse.jsx
@@ -2,59 +2,69 @@ import '../../services/Api/Api'
 import { Registro } from '../../services/Api/Api'
 import { departamento } from '../../services/Api/Api';
 import { ciudad } from '../../services/Api/Api';
-import { useRef, useState } from 'react'
-
-
+import { useRef, useState, useEffect } from 'react'
 
 const Registrarse = () => {
 
     const selectRef = useRef()
+    const [departamentos, setDeparamentos] = useState([]);
+    
+    useEffect(() => {
+        departamento
+          .then(data => {
+            setDeparamentos(data.departamentos)
+          })
+          .catch(e => console.error('Ha ocurrido un error'))
+      }, [])
 
     const onSelectChange = () => {
         const idDepartamento = Number(selectRef.current.value.id)
         onSelectCiudad(idDepartamento)
     }
     const onSelectCiudad = (idDepartamento) => {
-        ciudad(idDepartamento); 
+        ciudad(idDepartamento);
     }
-    
 
 
+    return (
+       
+        <form>
+            <div className="form-group">
+                <label htmlFor="exampleFormControlInput1">Usuario</label>
+                <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="usuario"></input>
+            </div>
+            <div className="form-group">
+                <label htmlFor="exampleFormControlInput1">Password</label>
+                <input type="password" className="form-control" id="idPassword" placeholder="Password"></input>
+            </div>
+            <div className="form-group">
+                <label htmlFor="exampleFormControlSelect1">Deparamento</label>
+                <select className="form-control" id="exampleFormControlSelect1">
 
-  return (
-<form>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Usuario</label>
-    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="usuario"></input>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Password</label>
-    <input type="password" class="form-control" id="idPassword" placeholder="Password"></input>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Deparamento</label>
-    <select class="form-control" id="exampleFormControlSelect1"
-    onChange={onSelectChange}
-    ref={selectRef}
-    value={departamento}>
-        
-      <option value={departamento}></option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect2">Ciudad</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2"
-    onChange={onSelectCiudad}>
-      <option value={ciudad}></option>
-    </select>
-  </div>
-  <div class="form-group">
-  <button type="submit" class="btn btn-primary">Registrarme</button>
-  </div>
-</form>
- 
-    
-  )
+                    {departamentos.map(({ idDepartamento, nombre }) => (
+            <option>
+              id={idDepartamento}
+              nombre={nombre}
+            
+              </option>
+            
+          ))}
+                </select>
+            </div>
+            <div className="form-group">
+                <label htmlFor="exampleFormControlSelect2">Ciudad</label>
+                <select multiple className="form-control" id="exampleFormControlSelect2"
+                    onChange={onSelectCiudad}>
+                    <option ></option>
+                </select>
+            </div>
+            <div className="form-group">
+                <button type="submit" className="btn btn-primary">Registrarme</button>
+            </div>
+        </form>
+
+
+    );
 };
 
 export default Registrarse
